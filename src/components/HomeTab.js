b4c068a7c724bfa0e5c997f7a37d946ed8028505
leejaerim <ger6608@naver.com>
2023-02-23 23:44:39 +0900

    #2 get the number of table in realtime. (v3)
    in TableCard component, it doesn't allow to modify value without onChange(or another) hook.

--- src/components/HomeTab.js
+++ src/components/HomeTab.js
@@ -1,21 +1,34 @@
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Table from "./Table";
 import {useOutletContext} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {collection, limit, onSnapshot, orderBy, query} from "firebase/firestore";
+import {dbService} from "../fbase";
 
 const HomeTab = ()=>{
     const [table_price] = useOutletContext();
+    const [tableCount, setTableCount] = useState(0);
+    useEffect(() => {
+        //getNweets();
+        onSnapshot(query(
+            collection(dbService, "table"),
+            orderBy("createAt", "desc"),
+            limit(1)
+        ), obj => {
+            setTableCount(obj.docs[0].data().number_of_table);
+        })
+    },[])
     return(
         <Tabs  isFitted variant='enclosed' variant='soft-rounded' colorScheme='blue'>
             <TabList>
-                <Tab>1</Tab>
-                <Tab>2</Tab>
-                <Tab>3</Tab>
-                <Tab>4</Tab>
+                {[ ...Array(tableCount).keys()].map( i =>(
+                    <Tab key={i}>{i+1}</Tab>
+                ))}
             </TabList>
             <TabPanels>
-                {[1,2,3,4].map((index)=>(
-                    <TabPanel key={index}>
-                        <Table  table_price={table_price}></Table>
+                {[ ...Array(tableCount).keys()].map( i =>(
+                    <TabPanel key={i}>
+                        <Table index={i} table_price={table_price}></Table>
                     </TabPanel>
                 ))}
             </TabPanels>
